dfd48e6bc5b5ab8761e193f894269281fec27ede
Jennifer Perez <perez_jennifer_ramer@student.smc.edu>
2019-03-18 21:41:22 -0400

    Initial commit

---
+++ app/src/main/java/edu/smc/perez18/mysqlexampleproject/DbStrings.java
@@ -0,0 +1,10 @@
+package edu.smc.perez18.mysqlexampleproject;
+
+public class DbStrings {
+
+    static final String DATABASE_URL = "10.0.0.60:3306";
+    static final String DATABASE_NAME = "fruits_example";
+    static final String USERNAME = "some_user";
+    static final String PASSWORD = "your_password";
+
+}
